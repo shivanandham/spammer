a7f4c5e3c5cf718c72b30e9d58ddf1f0200b75f9
Shiva <shivanandham625@gmail.com>
2020-11-02 18:51:09 +0530

    optimised for whats app

--- script.py
+++ script.py
@@ -2,5 +2,7 @@ import pyautogui, time
 time.sleep(5)
 f = open("YourTextHere.txt", 'r')
 for i in f:
-    pyautogui.typewrite(i)
+    if len(i)>3:
+        pyautogui.typewrite(i)
+        time.sleep(0.3)
 pyautogui.press("enter")
